c0d701a3a139137cfe7b521ec70dd1fdd568c5dc
Ubuntu <ubuntu@ip-172-31-22-77.ec2.internal>
2024-06-10 07:16:24 +0000

    Added File FindFile.sh

---
+++ FindFile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+########################################################
+#
+#Author: Rahul Soni
+#Date: 17th May 2024
+#Version: V1.0
+#Description: To find location of a file in system
+#
+########################################################
+#
+
+
+echo ""
+
+set -x
+
+echo "Search a File and provide File Location"
+
+find ~/ -type f -name "NodeHealth.sh"
+
+echo ""
+
